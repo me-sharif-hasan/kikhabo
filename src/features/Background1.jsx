c66081556aba3fbcb4383d778613b0a39b1ecd8c
jannatulferdos05 <jannatulferdos587@gmail.com>
2024-07-10 14:37:02 +0600

    login-2nd-inc

--- src/features/Background1.jsx
+++ src/features/Background1.jsx
@@ -4,7 +4,7 @@ import LoginPage from './login/screens/LoginPage'
 
 const Background1 = () => {
   return ( 
-      <div className="bg-bgImage  w-full h-screen bg-no-repeat bg-center">
+      <div className="text-black-900 bg-bgImage  w-full h-[100vh] flex justify-center items-center bg-no-repeat bg-center">
         <LoginPage/>
       </div>
   )
--- src/features/login/screens/LoginPage.jsx
+++ src/features/login/screens/LoginPage.jsx
@@ -3,21 +3,23 @@ import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
   return (
-    <div className="flex items-center h-96">
-        <div className="h-96 w-1/2 mx-auto my-auto bg-slate-800 border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative flex justify-center items-center align-middle ">
-            <h1 className="mt-8" >Login</h1>
+    <div className=" ">
+        <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-10 relative">
+            
+            <h1 className="text-4xl text-black font-bold text-center mb-6" >Login</h1>
+            
             <form action="">
-            <div>
-                <input type="email"/>
-                <label htmlFor=''>Your Email</label>
+            <div className="relative my-4 " >
+                <input className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer " type="email"/>
+                <label className="absolute text-sm text-black duration-300 transform-translate-y-6 scale-75 top-3-z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-6 " htmlFor=''>Your Email</label>
             </div>
-            <div>
-                <input type="password"/>
-                <label htmlFor=''>Your Password</label>
+            <div className="relative my-4">
+                <input className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer " type="password"/>
+                <label className="absolute text-sm text-black duration-300 transform-translate-y-6 scale-75 top-3-z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-6 " htmlFor=''>Your Password</label>
             </div>
             <div>
                 <div>
-                <input type="checkbox" name="" id="" />
+                <input  type="checkbox" name="" id="" />
                 <label htmlFor='Remember Me'>Remember Me</label>
                 </div>
                 <span>Forget password ?</span>
