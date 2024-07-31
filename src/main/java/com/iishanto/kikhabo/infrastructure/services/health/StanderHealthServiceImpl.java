fa74193632e7601921ccbeb99fec73948b1d506c
Sharif Hasan <me.sharif.hasan@gmail.com>
2024-08-01 02:31:20 +0600

    fix(bmi)

--- src/main/java/com/iishanto/kikhabo/infrastructure/services/health/StanderHealthServiceImpl.java
+++ src/main/java/com/iishanto/kikhabo/infrastructure/services/health/StanderHealthServiceImpl.java
@@ -6,9 +6,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class StanderHealthServiceImpl implements HealthServices{
     public double getBMI(User user){
-        float userHeight= user.getHeightInFt();
-        float userWeight=user.getWeightInKg();
-        var height=userHeight*0.3084;
-        return userWeight/(height*height);
+        try{
+            float userHeight= user.getHeightInFt();
+            float userWeight=user.getWeightInKg();
+            var height=userHeight*0.3084;
+            return userWeight/(height*height);
+        }catch (Exception e){
+            return 20;
+        }
     }
 }
