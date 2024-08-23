5b8712689fbc1b6213913b7179672167a91930ca
jannatulferdos05 <jannatulferdos587@gmail.com>
2024-08-23 23:11:01 +0600

    submit-issue

--- src/features/dashboard/screens/navbar/preferences-manage/ManagePrefrences.jsx
+++ src/features/dashboard/screens/navbar/preferences-manage/ManagePrefrences.jsx
@@ -15,11 +15,24 @@ const ManagePrefrences = () => {
   const [loading, setLoading] = useState(false);
   const [loadData, setLoadData] = useState(false);
 
+  const [spicyRating, setSpicyRating] = useState('');
+  const [priceRating, setPriceRating] = useState('');
+  const [saltRating, setSaltRating] = useState('');
+  const [hasDiabets, setHasDiabets] = useState('');
+  const [isPregnant, setIsPregnant] = useState('');
+  const [specialNotes, setSpecialNotes] = useState('');
+
   useEffect(() => {
     user.currentUser().then((response) => {
       console.log(response);
       if (response.status === 'success') {
           setCurrentUser(response.data);
+          setSpicyRating(response.data?.preference?.spicyRating || '');
+          setPriceRating(response.data?.preference?.budgetRating || '');
+          setSaltRating(response.data?.preference?.saltTasteRating || '');
+          setHasDiabets(response.data?.preference?.hasDiabetics? 'Yes' : 'No');
+          setIsPregnant(response.data?.preference?.pregnant ? 'Yes' : 'No');
+          setSpecialNotes(response.data?.preference?.specialNotes || '');
       }
   }).catch((error) => {
       console.log(error);
@@ -42,20 +55,16 @@ const ManagePrefrences = () => {
     openModal(); };
 
   const handleSubmit = (e) => {
+    console.log('Form is eagerlyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy submitted');
     e.preventDefault();
     console.log('Form is submitted');
-        let spicyRating = e.target[0].value;
-        let priceRating = e.target[1].value;
-        let saltRating = e.target[2].value;
-        let hasDiabets = e.target[3].value;
-        let isPregnant = e.target[4].value;
-        let specialNotes = e.target[5].value;
         setLoading(true);
         preference.setPreference(spicyRating,priceRating,saltRating,hasDiabets,isPregnant, specialNotes).then((response) => {
             console.log(response);
             if (response.status === 'success') {
                 setLoading(false);
-                <h1>Congratulations!! Your preferences are saved . </h1>
+                setButton('Update Preferences');
+                console.log('Preference added successfully');
                 setLoadData(!loadData);
                 closeModal();
             }
@@ -69,20 +78,19 @@ const ManagePrefrences = () => {
   return (
     <div>
       <div className='flex relative'>
-        <p>JSON to stringfy {JSON.stringify(current_user?.preference)} </p>
+        <p>{JSON.stringify(current_user?.preference)} </p>
         
 
       {!current_user?.preference ? (
         <h1 className='h-52 mt-0 fixed' >You have no preferences!! Unbelievable!! Please add your preferences.</h1>
       ) : (
         <Preferences />   
-      )
-      }
+      )}
       </div>
       <div className='flex relative mt-96 ml-96 align-middle '>
       <button onClick={handleClick} className="relative w-52 text-[18px] rounded-full bg-emerald-700 hover:bg-white py-2 transition-colors duration-300" >{pbutton}</button>
       <Modal className="flex flex-crelativeol} justify-center items-center bg-white shadow-md border border-gray-300 rounded-xl p-4 absolute fit-content w-[400px] h-[400px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-      isOpen={modalIsOpen}
+      isOpen={modalIsOpen} ariaHideApp={false}
       onRequestClose={closeModal} 
       >
         <div className="flex relative flex-col justify-center items-center align-middle backdrop-filter backdrop-blur-sm">
@@ -91,29 +99,38 @@ const ManagePrefrences = () => {
                 <h1 className='font-[Poppins] font-bold mt-6 ml-12 pb-5 text-gray-700 '>Add Your Preferences</h1>
                 {/* Startting of the input fields */}
                 <div className="relative my-4 " >
-                <input   className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer " value={!current_user?.preference?.spicyRating ? ('' ) : ( current_user?.preference?.spicyRating)} type="number"/>
+                <input   className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer " 
+                value={spicyRating} onChange={(e) => setSpicyRating(e.target.value)} type="number"/>
                 <label className=" absolute text-xl text-gray-700 transform scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-focus:scale-75 -translate-y-8 duration-500" htmlFor=''>Rate your spicy taste</label>
             </div>
             <div  className="relative my-4">
-                <input   className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer " value={!current_user?.preference?.budgetRating ? ('') : ( current_user?.preference?.budgetRating)} type="number"/>
+                <input   className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer " 
+                value={priceRating} onChange={(e) => setPriceRating(e.target.value)} type="number"/>
                 <label className="absolute text-xl text-gray-700 transform scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-focus:scale-75 -translate-y-8 duration-500" htmlFor=''>Suitable Price Rate</label>
             </div>
 
             <div className="relative my-4 " >
-                <input   className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer " value={!current_user?.preference?.saltTasteRating ? ('') : ( current_user?.preference?.saltTasteRating)} type="number"/>
+                <input   className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer " 
+                value={saltRating} onChange={(e)=> setSaltRating(e.target.value)} type="number"/>
                 <label className=" absolute text-xl text-gray-700 transform scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-focus:scale-75 -translate-y-8 duration-500" htmlFor=''>Rate your salty taste</label>
             </div>
             <div  className="relative my-4">
-                <input   className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer " value={!current_user?.preference?.hasDiabetics ? ('') : ( current_user?.preference?.hasDiabetics)} type="number"/>
+            <select className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer"
+               value={hasDiabets} onChange={(e) => setHasDiabets(e.target.value)} > <option value="true">Yes</option> <option value="false">No</option>
+             </select>
                 <label className="absolute text-xl text-gray-700 transform scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-focus:scale-75 -translate-y-8 duration-500" htmlFor=''>Have you Diabetics ?</label>
             </div>
 
-            <div className="relative my-4 " >
-                <input   className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer " value={!current_user?.preference?.pregnant ? ('' ) : ( current_user?.preference?.pregnant)} type="text"/>
-                <label className=" absolute text-xl text-gray-700 transform scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-focus:scale-75 -translate-y-8 duration-500" htmlFor=''>Are you Pregnant ?</label>
+            <div className="relative my-4">
+               <select className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer"
+               value={isPregnant} onChange={(e) => setIsPregnant(e.target.value)} > <option value="true">Yes</option> <option value="false">No</option>
+             </select>
+            <label className="absolute text-xl text-gray-700 transform scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-focus:scale-75 -translate-y-8 duration-500">Are you Pregnant? </label>
             </div>
+
             <div  className="relative my-4">
-                <input   className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer " value={!current_user?.preference?.specialNotes ? ('' ) : ( current_user?.preference?.specialNotes)} type="text"/>
+                <input   className="block w-72 py-2.3 px-0 text-sm text-black bg-transparent border-0 border-b-2 border-green-800 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-black focus:border-green-600 peer " 
+                value={specialNotes} onChange={(e) => setSpecialNotes(e.target.value)} type="text"/>
                 <label className="absolute text-xl text-gray-700 transform scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-focus:scale-75 -translate-y-8 duration-500" htmlFor=''>Any special notes ?</label>
             </div>
              {/* Ending of the input fields */}
